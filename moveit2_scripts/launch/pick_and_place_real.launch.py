9ebfc7b4d529d16dcf4f85034ac60f7b3856a28c
maxime.cognie <maxime.cognie@protonmail.com>
2024-06-18 10:58:43 +0000

    Task 2. pick and place script for the real ur3 arm

--- moveit2_scripts/launch/pick_and_place_real.launch.py
+++ moveit2_scripts/launch/pick_and_place_real.launch.py
@@ -3,14 +3,15 @@ from launch import LaunchDescription
 from launch_ros.actions import Node
 
 def generate_launch_description():
+
     # MoveItCpp demo executable
     moveit_cpp_node = Node(
-        name="pick_and_place",
+        name="pick_and_place_real",
         package="moveit2_scripts",
-        executable="pick_and_place",
+        executable="pick_and_place_real",
         output="screen",
         parameters=[
-            {'use_sim_time': False},
+            {'use_sim_time': True},
         ],
     )
 
--- moveit2_scripts/src/pick_and_place_real.cpp
+++ moveit2_scripts/src/pick_and_place_real.cpp
@@ -1,3 +1,4 @@
+#include "geometry_msgs/msg/detail/pose__struct.hpp"
 #include <moveit/move_group_interface/move_group_interface.h>
 #include <moveit/planning_scene_interface/planning_scene_interface.h>
 
@@ -5,14 +6,14 @@
 #include <moveit_msgs/msg/display_trajectory.hpp>
 #include <vector>
 
-static const rclcpp::Logger LOGGER = rclcpp::get_logger("pick_and_place");
+static const rclcpp::Logger LOGGER = rclcpp::get_logger("pick_and_place_real");
 
 int main(int argc, char **argv) {
   rclcpp::init(argc, argv);
   rclcpp::NodeOptions node_options;
   node_options.automatically_declare_parameters_from_overrides(true);
   auto move_group_node =
-      rclcpp::Node::make_shared("pick_and_place_node", node_options);
+      rclcpp::Node::make_shared("pick_and_place_real_node", node_options);
 
   rclcpp::executors::SingleThreadedExecutor executor;
   executor.add_node(move_group_node);
@@ -55,9 +56,7 @@ int main(int argc, char **argv) {
   bool success_arm;
   bool success_gripper;
 
-  geometry_msgs::msg::Pose target_pose;
-
-  move_group_arm.setNamedTarget("initial_pose");
+  move_group_arm.setNamedTarget("initial_position");
   success_arm = (move_group_arm.plan(my_plan_arm) ==
                      moveit::core::MoveItErrorCode::SUCCESS);
 
@@ -66,32 +65,50 @@ int main(int argc, char **argv) {
   // Pregrasp
   RCLCPP_INFO(LOGGER, "Pregrasp Position");
 
-  move_group_arm.setNamedTarget("pre_grasp_pose"); 
+  move_group_arm.setNamedTarget("pre_grasp_position"); 
 
   success_arm = (move_group_arm.plan(my_plan_arm) ==
                  moveit::core::MoveItErrorCode::SUCCESS);
 
+  std::this_thread::sleep_for(std::chrono::milliseconds(5000));
   move_group_arm.execute(my_plan_arm);
 
   // Open Gripper
   RCLCPP_INFO(LOGGER, "Open Gripper!");
 
   move_group_gripper.setNamedTarget("open");
-
   success_gripper = (move_group_gripper.plan(my_plan_gripper) ==
                           moveit::core::MoveItErrorCode::SUCCESS);
-
   move_group_gripper.execute(my_plan_gripper);
 
   // Approach
   RCLCPP_INFO(LOGGER, "Approach to object!");
-  
-  move_group_arm.setNamedTarget("grasp_pose"); 
 
-  success_arm = (move_group_arm.plan(my_plan_arm) ==
-                 moveit::core::MoveItErrorCode::SUCCESS);
+  geometry_msgs::msg::Pose target_pose;
+  std::vector<geometry_msgs::msg::Pose> approach_waypoints;
+  target_pose.position.x = 0.343;
+  target_pose.position.y = 0.132;
+  target_pose.position.z = 0.25;
+  target_pose.orientation.x = -1.0;
+  target_pose.orientation.y = 0.0;
+  target_pose.orientation.z = 0.0;
+  target_pose.orientation.w = 0.0;
+  approach_waypoints.push_back(target_pose);
 
-  move_group_arm.execute(my_plan_arm);
+  target_pose.position.z -= 0.025;
+  approach_waypoints.push_back(target_pose);
+
+  target_pose.position.z -= 0.025;
+  approach_waypoints.push_back(target_pose);
+
+  moveit_msgs::msg::RobotTrajectory trajectory_approach;
+  const double jump_threshold = 0.0;
+  const double eef_step = 0.01;
+
+  double fraction = move_group_arm.computeCartesianPath(
+      approach_waypoints, eef_step, jump_threshold, trajectory_approach);
+
+  move_group_arm.execute(trajectory_approach);
 
   // Close Gripper
   RCLCPP_INFO(LOGGER, "Close Gripper!");
@@ -105,14 +122,21 @@ int main(int argc, char **argv) {
 
   // Retreat
   RCLCPP_INFO(LOGGER, "Retreat from object!");
+  std::vector<geometry_msgs::msg::Pose> retreat_waypoints;
+
+  for (int i = 0; i < 4; i++){
+    target_pose.position.z += 0.025;
+    retreat_waypoints.push_back(target_pose);
+  }
+
+  moveit_msgs::msg::RobotTrajectory trajectory_reatreat;
+
+  fraction = move_group_arm.computeCartesianPath(
+      retreat_waypoints, eef_step, jump_threshold, trajectory_reatreat);
+
   std::this_thread::sleep_for(std::chrono::milliseconds(1000));
 
-  move_group_arm.setNamedTarget("pre_grasp_pose"); 
-
-  success_arm = (move_group_arm.plan(my_plan_arm) ==
-                 moveit::core::MoveItErrorCode::SUCCESS);
-
-  move_group_arm.execute(my_plan_arm);
+  move_group_arm.execute(trajectory_reatreat);
 
   // Place
   RCLCPP_INFO(LOGGER, "Rotating Arm");
@@ -131,6 +155,40 @@ int main(int argc, char **argv) {
   move_group_arm.execute(my_plan_arm);
   std::this_thread::sleep_for(std::chrono::milliseconds(1000));
 
+  // Place
+  RCLCPP_INFO(LOGGER, "Rotating Arm");
+
+  current_state_arm = move_group_arm.getCurrentState(10);
+  current_state_arm->copyJointGroupPositions(joint_model_group_arm,
+                                             joint_group_positions_arm);
+
+  joint_group_positions_arm[0] -= 3.1415; // Shoulder Pan
+
+  move_group_arm.setJointValueTarget(joint_group_positions_arm);
+
+  success_arm = (move_group_arm.plan(my_plan_arm) ==
+                 moveit::core::MoveItErrorCode::SUCCESS);
+
+  move_group_arm.execute(my_plan_arm);
+  std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+
+  RCLCPP_INFO(LOGGER, "Releasing the block");
+  std::vector<geometry_msgs::msg::Pose> release_waypoints;
+
+  for (int i = 0; i < 4; i++){
+    target_pose.position.z -= 0.025;
+    release_waypoints.push_back(target_pose);
+  }
+
+  moveit_msgs::msg::RobotTrajectory trajectory_release;
+
+  fraction = move_group_arm.computeCartesianPath(
+      release_waypoints, eef_step, jump_threshold, trajectory_release);
+
+  std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+
+  move_group_arm.execute(trajectory_release);
+
   // Open Gripper
   RCLCPP_INFO(LOGGER, "Release Object!");
 
@@ -144,12 +202,14 @@ int main(int argc, char **argv) {
   // Go back to arm initial position
   RCLCPP_INFO(LOGGER, "Move back to initial position!");
   std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-  move_group_arm.setNamedTarget("initial_pose");
+  move_group_arm.setNamedTarget("initial_position");
 
   success_arm = (move_group_arm.plan(my_plan_arm) ==
                      moveit::core::MoveItErrorCode::SUCCESS);
 
   move_group_gripper.execute(my_plan_arm);
+
+  RCLCPP_INFO(LOGGER, "Pick and place mission done!");
 
   rclcpp::shutdown();
   return 0;
